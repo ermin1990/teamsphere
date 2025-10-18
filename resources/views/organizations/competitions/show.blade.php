9ecc060f52d34b3cb8d272b4de4665b85b2511af
ermin1990 <ermin1990@gmail.com>
2025-10-18 18:45:09 +0200

    refactor: remove public visibility toggle from competition show page
    - Remove Public Visibility Toggle switch from quick actions bar
    - Clean up competition show page layout

--- resources/views/organizations/competitions/show.blade.php
+++ resources/views/organizations/competitions/show.blade.php
@@ -105,21 +105,6 @@
                     Nazad na Organizaciju
                 </a>
 
-                {{-- Public Visibility Toggle --}}
-                <form method="POST" action="{{ route('organizations.competitions.update', [$organization, $competition]) }}" class="inline-flex items-center space-x-2">
-                    @csrf
-                    @method('PATCH')
-                    <label class="flex items-center space-x-2 cursor-pointer">
-                        <span class="text-white text-sm font-medium">{{ __('Public') }}</span>
-                        <input type="checkbox" 
-                               name="is_public" 
-                               value="1" 
-                               {{ $competition->isPublic() ? 'checked' : '' }}
-                               class="sr-only peer"
-                               onchange="this.form.submit()">
-                        <div class="w-11 h-6 bg-gray-600 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
-                    </label>
-                </form>
                 
                                 @if($competition->status === 'draft')
                 <form action="{{ route('organizations.competitions.destroy', [$organization, $competition]) }}" 
